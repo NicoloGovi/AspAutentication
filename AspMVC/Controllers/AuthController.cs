5d5b4530bb17f6c2336eed71f778c4b9cdb29d17
dluppoli <davide@davideluppoli.it>
2022-12-12 13:56:49 +0100

    Add project files.

---
+++ AspMVC/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using AspMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AspMVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login( UserCredentialsDto credentials)
+        {
+            using(var context = new UsersContext())
+            {
+                bool ok = context.Users.Where(q => q.Username.ToLower() == credentials.Username.ToLower() && q.Password == credentials.Password).Count() == 1;
+                if (ok)
+                {
+                    FormsAuthentication.SetAuthCookie(credentials.Username, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                TempData["Error"] = "Username o password errati!";
+                return RedirectToAction("Login");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ AspMVC/Dtos/UserCredentialsDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspMVC.Models
+{
+    public class UserCredentialsDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [MinLength(4)]
+        public string Password { get; set; }
+    }
+}
